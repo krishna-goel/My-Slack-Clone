3f1e0f201be5b400ec04c898269c13ba2c6d77e6
krishna-goel <krishnagoel578@gmail.com>
2021-05-14 12:12:46 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div>
+      <h3>hello :rocket</h3>
+      <Header/>
+      {/* Header */}
+      {/* Slider */}
+      {/* React-Router -> Chat Screen switch between channels without refreshing */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import "./Header.css";
+import { Avatar } from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <Avatar
+                    className="header_avatar"
+                    alt="hello"
+                    src=""
+                />
+                {/* Avatars for logged in user */}
+                <AccessTimeIcon />
+                {/* Time icon */}
+            </div>
+            <div className="header__search">
+                <SearchIcon />
+                {/* search icon */}
+                <input placeholder="search slack"/>
+                {/* input */}
+            </div>
+            <div className="header__right">
+                <HelpOutlineIcon />
+                {/* help icon */}
+            </div>
+        </div>
+    )
+}
+
+export default Header
